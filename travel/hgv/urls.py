e8fe178c6c9d18abff29ca99f86ce5eca174042e
risvana <vkrisvanarisu@gmail.com>
2023-04-04 21:36:26 +0530

    ok

--- travel/hgv/urls.py
+++ travel/hgv/urls.py
@@ -1,5 +1,15 @@
 from django. urls import path
 from . import views
+
+
 urlpatterns = [
-    path('index3',views.registerhgv,name="index3")
+    path('homepage1',views.gethomepage1,name="homepage1"),
+    path('guidehome',views.getguidehome,name="guidehome"),
+    path('hotelhome',views.gethotelhome,name="hotelhome"),
+    path('login1',views.getlogin1,name="login1"),
+    path('signup1',views.getsignup1,name="signup1"),
+    path('vehiclehome',views.getvehiclehome,name="vehiclehome"),
+    path('home1',views.gethome1,name="home1"),
 ]
+
+
--- travel/hgv/views.py
+++ travel/hgv/views.py
@@ -2,5 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def registerhgv(request):
-    return render(request,"hgv/index3.html")
+def gethomepage1(request):
+    return render(request,"hgv/homepage1.html")
+
+def getguidehome(request):
+    return render(request,"hgv/guidehome.html")
+
+def gethotelhome(request):
+    return render(request,"hgv/hotelhome.html")
+
+def getlogin1(request):
+    return render(request,"hgv/login1.html")
+
+def getsignup1(request):
+    return render(request,"hgv/signup1.html")
+
+def getvehiclehome(request):
+    return render(request,"hgv/vehiclehome.html")
+
+def gethome1(request):
+    return render(request,"hgv/home1.html")
+
